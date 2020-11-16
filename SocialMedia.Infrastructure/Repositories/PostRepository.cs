afda270a16fb9eac4be18ca2871f9e9b226799a7
bernie Ramone <bernie_ramone@outlook.com>
2020-11-15 21:15:56 -0600

    implement architecture

---
+++ SocialMedia.Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,24 @@
+﻿using SocialMedia.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SocialMedia.Infrastructure.Repositories
+{
+    public class PostRepository
+    {
+        public IEnumerable<Post> GetPosts()
+        {
+            IEnumerable<Post> posts = Enumerable.Range(1, 35).Select(x => new Post { 
+             PostId = x,
+             Description = $"Description {x}",
+             Date = DateTime.Now,
+             Image = $"https://misapis.com/{x}",
+             UserId = x
+            });
+
+            return posts;
+        }
+            
+    }
+}
