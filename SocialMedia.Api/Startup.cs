5706f1ce41d44e3acfdca6613dfe1e490eb4647c
bernie Ramone <bernie_ramone@outlook.com>
2020-11-15 21:35:25 -0600

    implementation of dependency injection

--- SocialMedia.Api/Startup.cs
+++ SocialMedia.Api/Startup.cs
@@ -1,15 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using SocialMedia.Core.Interfaces;
+using SocialMedia.Infrastructure.Repositories;
 
 namespace SocialMedia.Api
 {
@@ -26,6 +21,11 @@ namespace SocialMedia.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            //Dependency Injection
+            services.AddTransient<IPostRepository, PostMongoRepository>();
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- SocialMedia.Infrastructure/Repositories/PostRepository.cs
+++ SocialMedia.Infrastructure/Repositories/PostRepository.cs
@@ -1,13 +1,15 @@
 ﻿using SocialMedia.Core.Entities;
+using SocialMedia.Core.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace SocialMedia.Infrastructure.Repositories
 {
-    public class PostRepository
+    public class PostRepository : IPostRepository
     {
-        public IEnumerable<Post> GetPosts()
+        public async Task<IEnumerable<Post>> GetPosts()
         {
             IEnumerable<Post> posts = Enumerable.Range(1, 35).Select(x => new Post { 
              PostId = x,
@@ -16,6 +18,8 @@ namespace SocialMedia.Infrastructure.Repositories
              Image = $"https://misapis.com/{x}",
              UserId = x
             });
+
+            await Task.Delay(10);
 
             return posts;
         }
